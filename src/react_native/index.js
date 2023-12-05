279ce8b100db0c01f475a20ececfba0ba793386a
Mohit Tejani <mohit.tejani@pubnub.com>
2023-12-05 12:51:31 +0530

    added crypto support for react native environment

--- src/react_native/index.js
+++ src/react_native/index.js
@@ -3,6 +3,8 @@ import { Buffer } from 'buffer';
 import PubNubCore from '../core/pubnub-common';
 import Networking from '../networking';
 import { decode } from '../core/components/base64_codec';
+import { TextDecoder, TextEncoder } from 'text-encoding-utf-8';
+import 'react-native-get-random-values';
 import { stringifyBufferKeys } from '../core/components/stringify_buffer_keys';
 import Cbor from '../cbor/common';
 import { del, get, post, patch } from '../networking/modules/web-node';
@@ -10,8 +12,17 @@ import { getfile, postfile } from '../networking/modules/react_native';
 
 import PubNubFile from '../file/modules/react-native';
 import { stringifyBufferKeys } from '../web';
+import { CryptoModule, LegacyCryptor, AesCbcCryptor } from '../crypto/modules/WebCryptoModule/webCryptoModule';
 
 global.Buffer = global.Buffer || Buffer;
+
+
+Object.assign(global, {
+  TextEncoder: TextEncoder,
+  TextDecoder: TextDecoder,
+});
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 export default class extends PubNubCore {
   constructor(setup) {
@@ -29,7 +40,15 @@ export default class extends PubNubCore {
     });
     setup.sdkFamily = 'ReactNative';
     setup.ssl = true;
-
+    setup.initCryptoModule = (cryptoConfiguration) => {
+      return new CryptoModule({
+        default: new LegacyCryptor({
+          cipherKey: cryptoConfiguration.cipherKey,
+          useRandomIVs: cryptoConfiguration.useRandomIVs,
+        }),
+        cryptors: [new AesCbcCryptor({ cipherKey: cryptoConfiguration.cipherKey })],
+      });
+    };
     super(setup);
   }
 }
